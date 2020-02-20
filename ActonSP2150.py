d542cedb50f8f90d9d6c17bd89bd4b37b6542aa1
jmball <jmball@users.noreply.github.com>
2020-02-20 14:39:10 +0000

    addressed linter warnings

--- ActonSP2150.py
+++ ActonSP2150.py
@@ -1,69 +1,74 @@
-# created by James Ball, 2019-10-10
+"""Acton SP2150 monochromator control library."""
 
 import visa
 
 
 class sp2150:
+    """Monochromator instrument object."""
+
     def __init__(self, addr, timeout=10000):
-        """Default termination char settings for PyVISA are compatible with RS232"""
+        """Instantiate PyVISA instrument object.
+
+        Default termination char settings for PyVISA are compatible with RS232.
+        """
         rm = visa.ResourceManager()
         self.instr = rm.open_resource(addr)
         self.instr.timeout = timeout
 
     def set_scan_speed(self, speed):
-        """Set grating scan speed in nm/min"""
+        """Set grating scan speed in nm/min."""
         return self.instr.query(f"{float(speed):.1f} NM/MIN")
 
     def get_scan_speed(self):
-        """Get grating scan speed in nm/min"""
+        """Get grating scan speed in nm/min."""
         return self.instr.query("?NM/MIN")
 
     def scan_to_wavelength(self, wavelength):
-        """Scan grating to wavelength in nm"""
+        """Scan grating to wavelength in nm."""
         return self.instr.query(f"{float(wavelength):.1f} NM")
 
     def goto_wavelength(self, wavelength):
-        """Go to grating position for wavelength in nm"""
+        """Go to grating position for wavelength in nm."""
         return self.instr.query(f"{float(wavelength):.1f} GOTO")
 
     def get_wavelength(self):
-        """Get current grating wavelength position in nm"""
+        """Get current grating wavelength position in nm."""
         return self.instr.query("?NM")
 
     def set_grating(self, grating):
-        """Set grating number"""
+        """Set grating number."""
         return self.instr.query(f"{int(grating)} GRATING")
 
     def get_grating(self):
-        """Get grating number"""
+        """Get grating number."""
         return self.instr.query("?GRATING")
 
     def set_turret(self, turret):
-        """Set turrent number"""
+        """Set turrent number."""
         return self.instr.query(f"{int(turret)} TURRET")
 
     def get_turret(self):
-        """Get turret number"""
+        """Get turret number."""
         return self.instr.query("?TURRET")
 
     def get_grating_info(self):
-        """Get groove spacing and blaze wavelength of each grating"""
+        """Get groove spacing and blaze wavelength of each grating."""
         return self.instr.query("?GRATINGS")
 
     def get_turret_info(self):
-        """Get groove spacing of each grating on each turret"""
+        """Get groove spacing of each grating on each turret."""
         return self.instr.query("?TURRETS")
 
     def set_filter(self, filter_pos):
-        """Set filter wheel position number"""
+        """Set filter wheel position number."""
         return self.instr.query(f"{int(filter_pos)} FILTER")
 
     def get_filter(self):
-        """Get filter wheel position number"""
+        """Get filter wheel position number."""
         return self.instr.query("?FILTER")
 
     def home_filter(self):
-        """Set filter wheel to home position"""
+        """Set filter wheel to home position."""
         return self.instr.query("FHOME")
 
 
@@ -102,7 +107,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # create instrument instance
-    mono = ActonSP2150(args.address)
+    mono = sp2150(args.address)
 
     # call function
     if args.function == "set_scan_speed":
@@ -141,4 +146,3 @@ if __name__ == "__main__":
         )
 
     print(resp.strip(" ok\r\n"))
-
